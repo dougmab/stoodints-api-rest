88498c97e84b26a1f72f8e5ac54d622b3bddf02c
Douglas Brum <douglas.mateus2005@gmail.com>
2022-01-30 11:20:22 -0300

    Add users index route

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,13 +1,25 @@
-const db = require('../models');
+const { User } = require('../models');
 
 class UserController {
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+
+      return res.json(users);
+    } catch (e) {
+      return res.status(500).json(null);
+    }
+  }
+
   async store(req, res) {
     try {
-      const newUser = await db.User.create(req.body);
-      res.status(200).json(newUser);
+      const newUser = await User.create(req.body);
+
+      return res.status(200).json(newUser);
     } catch (e) {
       const sqlErrors = e.errors.map((error) => error.message);
-      res.status(400).json({
+
+      return res.status(400).json({
         errors: sqlErrors,
       });
     }
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/User');
 
 const router = new Router();
 
+router.get('/', userController.index);
 router.post('/', userController.store);
 
 module.exports = router;
