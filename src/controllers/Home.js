0b594fc00c159085c6715a0b302c86bf1c6e65b6
Douglas Brum <douglas.mateus2005@gmail.com>
2022-02-01 22:17:21 -0300

    Remove model create, Home is useless now, #RIPBOZO

--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -1,23 +1,6 @@
-const db = require('../models');
-
 class HomeController {
-  async index(req, res) {
-    try {
-      const newStudent = await db.Student.create({
-        firstName: 'Miranda',
-        lastName: 'Souza',
-        email: 'mirandinha2000@gmail.com',
-        age: 22,
-        weight: 54.7,
-        height: 1.65,
-      });
-      res.status(200).json(newStudent);
-    } catch (e) {
-      console.log(e);
-      res.status(500).json({
-        error: 'Invalid parameters',
-      });
-    }
+  index(req, res) {
+    res.json('Index');
   }
 }
 
