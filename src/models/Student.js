40af4bcc5b54902d2a755a28a9353129f2cd34ef
Douglas Brum <douglas.mateus2005@gmail.com>
2022-02-02 18:52:18 -0300

    Add validation to model

--- src/models/Student.js
+++ src/models/Student.js
@@ -4,12 +4,58 @@ module.exports = (sequelize, DataTypes) => {
   class Student extends Model {}
 
   Student.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    age: DataTypes.INTEGER,
-    weight: DataTypes.FLOAT,
-    height: DataTypes.FLOAT,
+    firstName: {
+      type: DataTypes.STRING,
+      defaultValue: '',
+      validate: {
+        len: {
+          args: [3, 20],
+          msg: 'First name must be between 3 and 20 characters.',
+        },
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      defaultValue: '',
+      validate: {
+        len: {
+          args: [3, 20],
+          msg: 'Last name must be between 3 and 20 characters.',
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email.',
+        },
+      },
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: {
+          msg: 'Age must be an integer number.',
+        },
+      },
+    },
+    weight: {
+      type: DataTypes.FLOAT,
+      validate: {
+        isFloat: {
+          msg: 'Weight must be a float number.',
+        },
+      },
+    },
+    height: {
+      type: DataTypes.FLOAT,
+      validate: {
+        isFloat: {
+          msg: 'Weight must be a float number.',
+        },
+      },
+    },
   }, {
     sequelize,
     modelName: 'Student',
