79ce30285688b9334a916bda029766ade0893a13
Douglas Brum <douglas.mateus2005@gmail.com>
2022-02-02 23:32:48 -0300

    Add students CRUD

--- src/controllers/Student.js
+++ src/controllers/Student.js
@@ -5,6 +5,61 @@ class StudentController {
     const students = await Student.findAll();
     res.status(200).json(students);
   }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      const student = await Student.findByPk(id);
+
+      res.status(200).json(student);
+    } catch (e) {
+      res.status(500).json({ errors: ['Something went wrong with the database.'] });
+    }
+  }
+
+  async store(req, res) {
+    try {
+      const newStudent = await Student.create(req.body);
+
+      res.status(200).json(newStudent);
+    } catch (e) {
+      const sqlErrors = e.errors.map((err) => err.message);
+      if (!sqlErrors[0]) sqlErrors.push('Something went wrong with the database.');
+
+      res.status(500).json({ errors: sqlErrors });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      const student = await Student.findByPk(id);
+
+      const updatedStudent = await student.update(req.body);
+
+      res.status(200).json(updatedStudent);
+    } catch (e) {
+      const sqlErrors = e.errors.map((err) => err.message);
+      if (!sqlErrors[0]) sqlErrors.push('Something went wrong with the database.');
+
+      res.status(500).json({ errors: sqlErrors });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      const student = await Student.findByPk(id);
+
+      await student.destroy();
+      res.status(200).json(student);
+    } catch (e) {
+      res.status(500).json({ errors: ['Something went wrong with the database.'] });
+    }
+  }
 }
 
 module.exports = new StudentController();
--- src/routes/student.js
+++ src/routes/student.js
@@ -1,8 +1,16 @@
 const { Router } = require('express');
 const studentController = require('../controllers/Student');
+const loginReq = require('../middlewares/loginRequired');
 
 const router = new Router();
 
 router.get('/', studentController.index);
+router.get('/:id', studentController.show);
+
+router.post('/', loginReq, studentController.store);
+
+router.put('/:id', loginReq, studentController.update);
+
+router.delete('/:id', loginReq, studentController.delete);
 
 module.exports = router;
